d725a2246ae6901d820be8dc7bef2e1e24904a2c
Elvis Rodriguez <elvisrodriguez0621@gmail.com>
2021-07-29 03:19:27 -0400

    header changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () =>  (
   <Container>
         <Div1>
             <Link href="/">
-                  <a style={{display:"flex",alignItems:"center",color:"white", marginBottom:"20"}}>
+                  <a style={{display:"flex",alignItems:"center",color:"white", marginBottom:"20px"}}>
                       <DiCssdeck size="3rem"/> <Span>Porfolio</Span>
                   </a>
             </Link>
