5adf4f8117398766e48c7bcb24248f257f69c868
Elvis Rodriguez <elvisrodriguez0621@gmail.com>
2021-07-30 21:27:50 -0400

    title change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,12 @@
 import Theme from '../styles/theme';
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>My Portfolio</title>
+      </Head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
